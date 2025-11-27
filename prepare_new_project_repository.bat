30f47a9a0a11107b772e11803248c28c9ba87a51
ryanxely <ryanxely@gmail.com>
2025-11-27 19:03:45 +0100

    Added new project repos config script

---
+++ prepare_new_project_repository.bat
@@ -0,0 +1,7 @@
+@REM echo "# Project Name" >> README.md
+@REM git init
+@REM git add README.md
+@REM git commit -m "first commit"
+@REM git branch -M main
+@REM git remote add origin https://github.com/ryanxely/Project-Repo.git
+@REM git push -u origin main
