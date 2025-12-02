e8ac82dff4a2ba31c47b5d9ded78b4f80b76f799
ryanxely <ryanxely@gmail.com>
2025-12-02 20:15:34 +0100

    Prepared for final tests and deployment

--- config.py
+++
@@ -1,7 +0,0 @@
-REPORTS_FILE = "reports.json"
-USERS_FILE = "users.json"
-
-SMTP_SERVER = "smtp.galaxyconseils.com"
-TLS_PORT = 587
-ADMIN_EMAIL = "contact@galaxyconseils.com"
-ADMIN_EMAIL_PASSWORD = "Livre1234@"
--- tesr.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-files = [('files', open('E:/Music/Common Songs/06-MD-feat.-Niska.mp3','rb'))]
-data = {'text': 'Hello from python'}
-
-res = requests.post("http://localhost/post/add", data=data, files=files)
-print(res.json())
